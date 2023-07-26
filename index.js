700a4e200ab13297688be4a3a7cbd8a8d405dfd7
programmer-shaheb <abidurrahman471@gmail.com>
2023-07-26 12:33:54 +0600

    vercel file added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/api", routes);
-
+app.get("/", (req, res) => res.send("hello world"));
 const PORT = process.env.PORT || 5000;
 
 mongoose
